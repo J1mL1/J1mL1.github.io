82f5ae437463c03199370c6085c8b3188d2a85f2
zirui li <zirui_li_0806@outlook.com>
2025-11-02 00:28:06 +0800

    refactor: apply neumorphism to my website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'neu-base': '#e0e0e0',
+				'neu-light': '#ffffff',
+				'neu-dark': '#c0c0c0',
+				'neu-dark-base': '#2e2e2e',
+				'neu-dark-light': '#3a3a3a',
+				'neu-dark-dark': '#1a1a1a',
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-neumorphism'),
+	],
 }
