f694ac498bcbca8e5430b34fdf30ee738b46b8b3
J1mL1 <zirui_li_0806@outlook.com>
2026-05-17 12:18:24 +0800

    debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://j1ml1.github.io',
+  vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [
-    tailwind(),
     react(),
     sitemap()
   ],
